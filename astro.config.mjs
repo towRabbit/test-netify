188ccc4e1d5e8aaa8fd63a25bcbd8f204b1d6d0d
jizhiqiang <jizhiqiang@uino.com>
2022-05-14 20:15:54 +0800

    feat: 删除配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,12 +5,11 @@ import tailwind from "@astrojs/tailwind";
 import svelte from '@astrojs/svelte';
 import lit from '@astrojs/lit';
 // import svelte from '@astrojs/svelte';
-import netlify from '@astrojs/netlify/edge-functions';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [ svelte(), tailwind()],
-  adapter: netlify({
-    dist: new URL('./dist/', import.meta.url)
-  })
+  // adapter: netlify({
+  //   dist: new URL('./dist/', import.meta.url)
+  // })
 });
